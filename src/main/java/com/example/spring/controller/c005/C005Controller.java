ffea71ee3e24aee2754c9657ab7805364b8ecded
kuwalab <arata.shinichi@gmail.com>
2014-12-08 18:35:47 +0900

    005のメソッド名の修正

--- src/main/java/com/example/spring/controller/c005/C005Controller.java
+++ src/main/java/com/example/spring/controller/c005/C005Controller.java
@@ -9,18 +9,18 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/c005")
 public class C005Controller {
 	@RequestMapping(value = "/pathVar1/{foo}/{bar}", method = RequestMethod.GET)
-	public String pathVar3(@PathVariable String foo, @PathVariable String bar) {
+	public String pathVar1(@PathVariable String foo, @PathVariable String bar) {
 		return "c005/pathVar";
 	}
 
 	@RequestMapping(value = "/pathVar2/{bar1}/{foo1}", method = RequestMethod.GET)
-	public String pathVar4(@PathVariable("bar1") String bar,
+	public String pathVar2(@PathVariable("bar1") String bar,
 			@PathVariable("foo1") String foo) {
 		return "c005/pathVar";
 	}
 
 	@RequestMapping(value = "/pathVar3/{foo}/param/{bar}", method = RequestMethod.GET)
-	public String pathVar5(@PathVariable String foo, @PathVariable String bar) {
+	public String pathVar3(@PathVariable String foo, @PathVariable String bar) {
 		return "c005/pathVar";
 	}
 }
