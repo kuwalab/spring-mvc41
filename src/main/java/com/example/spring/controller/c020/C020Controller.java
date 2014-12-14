5996e2d2737ff1ff2f6070082bc9efbbd59587d7
kuwalab <arata.shinichi@gmail.com>
2014-12-14 12:47:00 +0900

    020

--- src/main/java/com/example/spring/controller/c020/C020Controller.java
+++ src/main/java/com/example/spring/controller/c020/C020Controller.java
@@ -1,10 +1,9 @@
 package com.example.spring.controller.c020;
 
-import javax.validation.Valid;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,7 +18,7 @@ public class C020Controller {
 	}
 
 	@RequestMapping(value = "/bookRecv", method = RequestMethod.POST)
-	public String bookRecv(@Valid @ModelAttribute C020Model c020Model,
+	public String bookRecv(@Validated @ModelAttribute C020Model c020Model,
 			BindingResult errors) {
 		if (errors.hasErrors()) {
 			return "c020/bookForm";
