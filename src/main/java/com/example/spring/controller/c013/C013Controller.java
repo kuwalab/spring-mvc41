3d8a5c6ecaa0f8785c8c0aa1a3a438aab4aef52f
kuwalab <arata.shinichi@gmail.com>
2014-12-14 12:23:39 +0900

    013の@Validを@Validatedに修正

--- src/main/java/com/example/spring/controller/c013/C013Controller.java
+++ src/main/java/com/example/spring/controller/c013/C013Controller.java
@@ -1,9 +1,8 @@
 package com.example.spring.controller.c013;
 
-import javax.validation.Valid;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,7 +16,7 @@ public class C013Controller {
 	}
 
 	@RequestMapping(value = "/bookRecv", method = RequestMethod.POST)
-	public String bookRecv(@Valid @ModelAttribute C013Model c013Model,
+	public String bookRecv(@Validated @ModelAttribute C013Model c013Model,
 			BindingResult errors) {
 		if (errors.hasErrors()) {
 			return "c013/bookForm";
