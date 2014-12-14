f0cbc2ecdce3a28ed75b3179fc3bfdc35853f873
kuwalab <arata.shinichi@gmail.com>
2014-12-14 12:41:26 +0900

    018 @Validを@Validatedに修正

--- src/main/java/com/example/spring/controller/c018/C018Controller.java
+++ src/main/java/com/example/spring/controller/c018/C018Controller.java
@@ -1,9 +1,8 @@
 package com.example.spring.controller.c018;
 
-import javax.validation.Valid;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,7 +16,7 @@ public class C018Controller {
 	}
 
 	@RequestMapping(value = "/bookRecv", method = RequestMethod.POST)
-	public String bookRecv(@Valid @ModelAttribute C018Model c018Model,
+	public String bookRecv(@Validated @ModelAttribute C018Model c018Model,
 			BindingResult errors) {
 		if (errors.hasErrors()) {
 			return "c018/bookForm";
