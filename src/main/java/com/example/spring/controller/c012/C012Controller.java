66ed894f4ac69630f370535886e28df43946e3fe
kuwalab <arata.shinichi@gmail.com>
2014-12-14 12:19:47 +0900

    012の@Validを@Validatedに修正

--- src/main/java/com/example/spring/controller/c012/C012Controller.java
+++ src/main/java/com/example/spring/controller/c012/C012Controller.java
@@ -1,9 +1,8 @@
 package com.example.spring.controller.c012;
 
-import javax.validation.Valid;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,7 +16,7 @@ public class C012Controller {
 	}
 
 	@RequestMapping(value = "/bookRecv", method = RequestMethod.POST)
-	public String bookRecv(@Valid @ModelAttribute C012Model c012Model,
+	public String bookRecv(@Validated @ModelAttribute C012Model c012Model,
 			BindingResult errors) {
 		if (errors.hasErrors()) {
 			return "c012/bookForm";
